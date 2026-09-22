e0ae5dfd627d1b8d85e02829a3b6438ddda051de
Sakib Hossain <sakibhossain368@gmail.com>
2026-09-22 18:39:20 +0100

    Time: 291 ms (42.59%) | Memory: 22.3 MB (13.65%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -5,21 +5,21 @@ class Solution:
 
 
         minHeap = [[0, 0, 0]] #[difference, row, col]
-        visit = set()
+        visited = set()
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
         while minHeap:
             diff, r, c = heapq.heappop(minHeap)
 
-            if (r, c) in visit:
+            if (r, c) in visited:
                 continue
-            visit.add((r, c))
+            visited.add((r, c))
             if  (r,c) == (rows - 1, cols - 1):
                 return diff
 
             for dr, dc in directions:
                 newRow, newCol = r + dr, c + dc
-                if (newRow < 0 or newCol < 0 or newRow == rows or newCol == cols or (newRow, newCol) in visit):
+                if (newRow < 0 or newCol < 0 or newRow == rows or newCol == cols or (newRow, newCol) in visited):
                     continue
                 newDiff = max(diff, abs(heights[r][c] - heights[newRow][newCol]))
                 heapq.heappush(minHeap, [newDiff, newRow, newCol])
